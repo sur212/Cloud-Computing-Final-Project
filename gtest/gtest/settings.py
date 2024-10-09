fb3cca159ccefcff8dba74dd4dfec0a8a0312767
ahs480 <ahs480@nyu.edu>
2024-10-09 12:08:17 -0400

    Update settings.py - removed KEY

--- gtest/gtest/settings.py
+++ gtest/gtest/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=*h6g2rgb_d#hv4locik6^0%gwzmp#4&obe#w%i))i%#-6j&1z'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
